c5a5f5abd3723b2d5b2f2e50221ed17e29ce3418
Dzmitry_A <abdiev.dn@yandex.by>
2020-11-01 12:13:15 +0300

    Update hanoi-tower.js

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -3,5 +3,5 @@ const CustomError = require("../extensions/custom-error");
 module.exports = 
 function calculateHanoi(disksNumber, turnsSpeed) {
   return {turns: 2 ** disksNumber - 1,
-        seconds: Math.floor((2 ** disksNumber - 1) / turnsSpeed * 360)};
+        seconds: Math.floor(((2 ** disksNumber - 1) / turnsSpeed * 60 ) * 60)};
 };
